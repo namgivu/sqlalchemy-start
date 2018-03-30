17c7c5061c4625666cca2f15984c4d460a27e8fe
namgivu <namgivu@gmail.com>
2018-03-30 22:46:01 +0700

    docs Why we use Column(MutableDict.as_mutable(JSONB)

--- lib/db/model/user.py
+++ lib/db/model/user.py
@@ -7,5 +7,13 @@ class User(DeclarativeBase):
     id           = Column(Integer, primary_key=True, autoincrement=True)
     email        = Column(String)
     name         = Column(String)
-    extra_info   = Column(MutableDict.as_mutable(JSONB), default={})
+
     custom_cols  = Column(JSONB, default={})
+    extra_info   = Column(MutableDict.as_mutable(JSONB), default={})
+    '''
+    Why we use Column(MutableDict.as_mutable(JSONB) but not plain `Column(JSONB)?
+    --> With plain JSONB, we need to handle the dictionary changes manually, 
+        and explicitly save the new changes because it cannot detect the changes automatically.
+        However, MutableDict can detect that a value within which has changed 
+        and automatically persist the changes to DB object
+    '''
