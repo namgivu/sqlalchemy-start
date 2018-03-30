9cc6a5850cb77d54972f8256479996ffa93f1cfc
namgivu <namgivu@gmail.com>
2018-03-30 15:04:06 +0700

    find_all()

--- lib/db/model/_base_.py
+++ lib/db/model/_base_.py
@@ -1,6 +1,13 @@
 class BaseModel(object):
+
+    @classmethod
+    def find_all(cls):
+        from lib.db.mysql import MySqlUtil
+        with MySqlUtil.get_session() as session:
+            rows = session.query(cls).all()
+            return rows
+
     #more common fields+methods among model classes to be here
-    pass
 
 from sqlalchemy.ext.declarative import declarative_base
 DeclarativeBase = declarative_base(cls=BaseModel)
--- task/t00_user_r.py
+++ task/t00_user_r.py
@@ -2,6 +2,15 @@ from lib.db.mysql       import *
 from lib.db.model._all_ import *
 from lib._common_       import *
 
-with MySqlUtil.get_session() as session:
-    users = session.query(User).all()
+
+def run01():
+    with MySqlUtil.get_session() as session:
+        users = session.query(User).all()
+        pprint(users)
+
+
+def run02():
+    users = User.find_all()
     pprint(users)
+
+run02()
