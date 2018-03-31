3f2673c561d3fbc9eabb65a8a6cbf632df126368
namgivu <namgivu@gmail.com>
2018-03-31 10:15:09 +0700

    get raw sql as util @ exp vs stm

--- lib/db/__init__.py
+++ lib/db/__init__.py
@@ -56,10 +56,26 @@ class DbUtil:
 
 
     @classmethod
-    def get_raw_sql(cls, sqlalchemy_expression, sql_dialect):
-        q   = sqlalchemy_expression #q means query
-        sql = str(q.statement.compile(dialect=sql_dialect)) #get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
-        return sql
+    def get_raw_sql(cls, sql_dialect, sqlalchemy_expression=None, sqlalchemy_statement=None):
+        """
+        :param sql_dialect:            choices    = DIALECT_POSTGRES | DIALECT_MYSQL
+        :param sqlalchemy_expression:  sample_exp = session.query(User).all()
+        :param sqlalchemy_statement:   sample_stm = session.update(User).values(name='abb')
+        :return:
+        """
+
+        if sqlalchemy_expression and sqlalchemy_statement:
+            raise Exception('Only $sqlalchemy_expression or $sqlalchemy_statement should be provided')
+
+        elif sqlalchemy_expression:
+            exp = sqlalchemy_expression #alias $exp means expression
+            sql = str(exp.statement.compile(dialect=sql_dialect)) #get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
+            return sql
+
+        elif sqlalchemy_statement is not None: #NOTE we have to use `is not None` here; otherwise, an error occurs 'TypeError: Boolean value of this clause is not defined'
+            stm = sqlalchemy_statement #alias $stm means statement
+            sql = str(stm.compile(dialect=sql_dialect, compile_kwargs={'literal_binds': True})) #get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
+            return sql
 
 
 ##region common import among model classes
@@ -68,11 +84,14 @@ class DbUtil:
 from lib.db.model._base_    import DeclarativeBase
 
 #table column type
-from sqlalchemy             import Column, UniqueConstraint, ForeignKey
-from sqlalchemy             import String, Integer, SmallInteger, BigInteger, DateTime, Float
+from sqlalchemy import Column, UniqueConstraint, ForeignKey
+from sqlalchemy import String, Integer, SmallInteger, BigInteger, DateTime, Float
+
+#crud command
+from sqlalchemy import update, insert
 
 #json column
-from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.ext.mutable              import MutableDict
 from sqlalchemy.dialects.postgresql.json import JSONB
 
 #sql dialect
--- task/t01_get_raw_sql_of_sqlalchemy_expression.py
+++ task/t01_get_raw_sql_of_sqlalchemy_expression.py
@@ -3,24 +3,69 @@ from lib.db.model._all_ import *
 from lib._common_       import *
 
 
-def run01():
+def run01a():
+    """
+    get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
+    simple query
+    """
     with DbUtil.get_session() as session:
         q = session.query(User)
         from sqlalchemy.dialects import postgresql
         sql=str(
             q.statement.compile(dialect=postgresql.dialect())
-        ) #get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
+        )
         print(sql)
 
 
-def run02():
+def run01b():
+    """
+    get a raw, compiled SQL query from a SQLAlchemy expression ref. https://stackoverflow.com/a/25563491/248616
+    simple query
+    """
     with DbUtil.get_session() as session:
         q = session.query(User)
-        sql_mysql    = DbUtil.get_raw_sql(q, DIALECT_MYSQL)
-        sql_postgres = DbUtil.get_raw_sql(q, DIALECT_POSTGRES)
+        sql_mysql    = DbUtil.get_raw_sql(sqlalchemy_expression=q, sql_dialect=DIALECT_MYSQL)
+        sql_postgres = DbUtil.get_raw_sql(sqlalchemy_expression=q, sql_dialect=DIALECT_POSTGRES)
         print('#sql_mysql   \n%s'% sql_mysql)
         print()
         print('#sql_postgres\n%s'% sql_postgres)
 
 
-run02()
+def run02a():
+    """
+    CRUD query
+    ref. https://stackoverflow.com/a/37842617/248616
+    """
+    q   = update(User).values(name='some name')
+    sql = q.compile(dialect=DIALECT_POSTGRES, compile_kwargs={"literal_binds": True})
+    print(sql)
+
+    q   = insert(User).values(name='some name')
+    sql = q.compile(dialect=DIALECT_POSTGRES, compile_kwargs={"literal_binds": True})
+    print(sql) #TODO how to limit users' columns listing to be exact as .values(col1='val1', ...) call?
+
+    '''sample output
+UPDATE users SET name='some name'
+INSERT INTO users (id, name, custom_cols, extra_info) VALUES (%(id)s, 'some name', %(custom_cols)s, %(extra_info)s) #TODO how to limit users' columns listing to be exact as .values(col1='val1', ...) call?
+    '''
+
+
+def run02b():
+    """
+    CRUD query
+    ref. https://stackoverflow.com/a/37842617/248616
+    """
+    q   = update(User).values(name='some name')
+    sql = DbUtil.get_raw_sql(sqlalchemy_statement=q, sql_dialect=DIALECT_POSTGRES)
+    print(sql)
+
+    q   = insert(User).values(name='some name')
+    sql = DbUtil.get_raw_sql(sqlalchemy_statement=q, sql_dialect=DIALECT_POSTGRES)
+    print(sql) #TODO how to limit users' columns listing to be exact as .values(col1='val1', ...) call?
+
+    '''sample output
+UPDATE users SET name='some name'
+INSERT INTO users (id, name, custom_cols, extra_info) VALUES (%(id)s, 'some name', %(custom_cols)s, %(extra_info)s) #TODO how to limit users' columns listing to be exact as .values(col1='val1', ...) call?
+    '''
+
+run02b()
