ccf3c232ead80b913cae8c9e5f4a78f852445546
namgivu <namgivu@gmail.com>
2018-04-01 18:37:56 +0700

    declarative query @ update via MutableDict

--- task/t03_jsonb_declarative_query.py
+++ task/t03_jsonb_declarative_query.py
@@ -5,9 +5,9 @@ from lib._common_       import *
 
 def seeding_data():
     engine.execute(delete(User))
-    u=User(**dict(name='1',  custom_cols={'k':'1'}));  DbUtil.insert(u)
-    u=User(**dict(name='22', custom_cols={'k':'22'})); DbUtil.insert(u)
-    u=User(**dict(name='22', custom_cols={'custom_cols_field':'ccf'})); DbUtil.insert(u)
+    u=User(**dict(name='1',   custom_cols={'k':'1'}));  DbUtil.insert(u)
+    u=User(**dict(name='22',  custom_cols={'k':'22'})); DbUtil.insert(u)
+    u=User(**dict(name='ccf', custom_cols={'custom_cols_field':'ccf'})); DbUtil.insert(u)
 
 def r01():
     seeding_data()
@@ -18,14 +18,22 @@ def r01():
 def r02():
     seeding_data()
     with DbUtil.get_session() as session:
-        print(); r=session.query(User.name, User.custom_cols).filter(User.custom_cols['custom_cols_field'].astext=='ccf').all(); print(r)
+        r=session.query(User.name, User.custom_cols).filter(User.custom_cols['custom_cols_field'].astext=='ccf').all(); print(r)
 
-        r=session.query(User).filter(User.custom_cols['custom_cols_field'].astext=='ccf').all()
+        #region update via .custom_cols['custom_cols_field']
+        r=session.query(User).filter(User.name=='ccf').all()
         assert len(r)>0 ; u=r[0]
-        u.custom_cols_field = 'new-new'
-        session.add(u); session.commit()
+        print(u.custom_cols['custom_cols_field'])
+        u.custom_cols['custom_cols_field'] = 'new new'; session.add(u); session.commit()
+        r=session.query(User.name, User.custom_cols).filter(User.name=='ccf').all(); print(r)
+        #endregion
 
-        #is not null ref. https://stackoverflow.com/a/21784915/248616
-        print(); r=session.query(User.name, User.custom_cols).filter(User.custom_cols['custom_cols_field'].astext.isnot(None)).all(); print(r)
+        #region update via .custom_cols_field
+        r=session.query(User).filter(User.name=='ccf').all()
+        assert len(r)>0 ; u=r[0]
+        print(u.custom_cols_field)
+        u.custom_cols_field = 'wen wen'; session.add(u); session.commit()
+        r=session.query(User.name, User.custom_cols).filter(User.name=='ccf').all(); print(r)
+        #endregion
 
 r02()
